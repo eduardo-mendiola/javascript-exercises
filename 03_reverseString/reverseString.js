51cc21c4f6028ace7411b8be366546fdc536c158
ezequielmz <93398008+ezequielmz@users.noreply.github.com>
2025-02-09 17:31:49 -0300

    Exercise 03_reverseString PASS

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+    let reverseText = "";
+    let numChar = text.length;
+    while (numChar > 0) {
+        reverseText += text[numChar-1];
+        numChar--
+    }
+    return reverseText;
 };
 
 // Do not edit below this line
