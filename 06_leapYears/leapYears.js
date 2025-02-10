75271b2ce639ae6997467fe11545838e9fe39035
ezequielmz <93398008+ezequielmz@users.noreply.github.com>
2025-02-09 21:27:04 -0300

    Exercise 06_leapYears PASS

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,27 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (year % 4 === 0 && year % 100 != 0) {
+        return true;
+    } else if (year % 4 === 0 && year % 100 === 0 && year % 400 === 0) {
+        return true;
+    } else {
+        return false;
+    }
 };
+
+// const leapYears = function (year) {
+//     const isYearDivisibleByFour = year % 4 === 0;
+//     const isCentury = year % 100 === 0;
+//     const isYearDivisibleByFourHundred = year % 400 === 0;
+  
+//     if (
+//       isYearDivisibleByFour &&
+//       (!isCentury || isYearDivisibleByFourHundred)
+//     ) {
+//       return true;
+//     } else {
+//       return false;
+//     }
+//   };
 
 // Do not edit below this line
 module.exports = leapYears;
