249ac96f224c69a12925abdd88e696ea2d411372
ezequielmz <93398008+ezequielmz@users.noreply.github.com>
2025-02-09 23:22:39 -0300

    Exercise 07_tempConversion PASS

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,22 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let conversion = (temp - 32) * (5/9);
+  let conversionRounded = Math.round(conversion * 10) / 10;
+  return conversionRounded;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let conversion = (temp * (9/5)) + 32;
+  let conversionRounded = Math.round(conversion * 10) / 10;
+  return conversionRounded;
 };
+
+// const convertToCelsius = function (fahrenheit) {
+//   return Math.round((fahrenheit - 32) * (5 / 9) * 10) / 10;
+// };
+
+// const convertToFahrenheit = function (celsius) {
+//   return Math.round(((celsius * 9) / 5 + 32) * 10) / 10;
+// };
 
 // Do not edit below this line
 module.exports = {
